20eb04417a6a0e5c93e41fb660ae0dc3c01bf8c2
HimanshiDeSilva <himanshidesilva2000@gmail.com>
2025-03-20 11:33:09 +0530

    modify default ports

--- Milestone 2/test_crypto_api.py
+++ Milestone 2/test_crypto_api.py
@@ -2,13 +2,13 @@ import requests
 import json
 
 # Define the URL of the endpoint
-url = 'http://127.0.0.1:5000/'
+url = 'https://en4720-production.up.railway.app/'
 
 # Generate an AES key
 payload = { "key_type": "AES",
 "key_size": 256 } 
 headers = { 'Content-Type': 'application/json' } 
-response = requests.post(url + "generate-key", data = json.dumps(payload), headers = headers) 
+response = requests.post(url + "generate-key", data = json.dumps(payload), headers = headers)  
 if response.status_code == 200: 
     data = response.json() 
     print("Key ID:", data.get("key_id")) 
@@ -17,7 +17,7 @@ else:
     print("Error:", response.json())
 
 # Encrypt a plaintext using the generated key
-key_id =  data.get("key_id")
+key_id =  response.json.get("key_id")
 plaintext = "Hello, AES encryption!"
 algorithm = "AES"
 payload = {
